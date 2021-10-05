0aae16e7878ac247897674f6b028945b8d6fb218
Vinicius Moreira <vinicius_fmoreira@hotmail.com>
2021-10-05 11:06:23 -0300

    [setup] updating description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 DESCRIPTION = (
-    "On-demand and/or auto performance optimizer for Linux applications."
+    "On-demand and auto performance optimizer for Linux applications."
 )
 
 AUTHOR = "Vinícius Moreira"
